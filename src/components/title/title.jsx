5697c408b22c398c43f8ab8518199b1781e22c34
Sergei Mikhailovskikh <for.mikhe@gmail.com>
2021-08-05 12:56:33 +0300

    : Add title component

---
+++ src/components/title/title.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createCn } from 'bem-react-classname';
+
+import './title.scss';
+import { Text } from '../../uikit';
+
+export const Title = () => {
+    const cn = createCn('title');
+
+    return (
+        <div className={cn()}>
+            <Text className={cn('text')} transform="uppercase" fs="86px">
+                Картины эпохи
+                <br />
+                Возрождения
+            </Text>
+        </div>
+    );
+};
