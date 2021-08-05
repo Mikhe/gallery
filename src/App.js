2be2bfe12a882ad5e35e644077ce3fa09fee14dd
Sergei Mikhailovskikh <for.mikhe@gmail.com>
2021-08-05 12:56:44 +0300

    : Add header & title to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import './styles/App.scss';
+import { Header } from './components/header/header';
+import { Title } from './components/title/title';
 
 function App() {
     return (
         <div className="App">
+            <Header />
+            <Title />
         </div>
   );
 }
