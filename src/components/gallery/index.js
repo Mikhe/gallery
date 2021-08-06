be53fb09ff7e9f382252d0319adf3b0ad7c04222
Sergei Mikhailovskikh <for.mikhe@gmail.com>
2021-08-06 14:42:54 +0300

    : Add content to Picture card

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -3,6 +3,7 @@ import { createCn } from 'bem-react-classname';
 
 import './gallery.scss';
 import mockData from '../../mock/data';
+import { Text } from '../../uikit';
 
 const rowHeight = 8;
 const columnWidth = 280;
@@ -31,16 +32,32 @@ const Gallery = () => {
 const Picture = (props) => {
     const { pic, cn } = props;
     const ref = React.useRef(null);
+    const prevPrice = pic.sale ? pic.price : '';
+    const currentPrice = pic.sale ? pic.sale : pic.price;
     const handleLoad = () => {
         resizeGridItem(ref.current);
     };
 
     return (
         <div ref={ref} className={cn('gallery-item')}>
-            <img src={pic.src} onLoad={handleLoad} />
+            <img src={pic.src} onLoad={handleLoad} className={cn('gallery-item-image')} />
             <div className={cn('gallery-item-info')}>
-                <div className={cn('gallery-item-info-title')}>{pic.title}</div>
-                <div className={cn('gallery-item-info-author')}>{pic.author}</div>
+                <div className={cn('gallery-item-info-wrap')}>
+                    <Text className={cn('gallery-item-info-title')} fs="18px">{pic.title}</Text>
+
+                    <Text className={cn('gallery-item-info-details')}>
+                        {`${pic.author}, ${pic.weight}`}
+                    </Text>
+
+                    <div className={cn('gallery-item-buy')}>
+                        <div className={cn('gallery-item-buy-price')}>
+                            <Text className={cn('gallery-item-buy-price-prev')}>{prevPrice}</Text>
+                            <Text className={cn('gallery-item-buy-price-current')} fs="16px">{currentPrice}</Text>
+                        </div>
+
+                        <button className={cn('gallery-item-buy-button')}>Купить</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
