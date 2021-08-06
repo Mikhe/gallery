be1624c0be7d1f6cdd6a3a51587878c2679b24d9
Sergei Mikhailovskikh <for.mikhe@gmail.com>
2021-08-06 07:22:09 +0300

    : Add Icon & TextInput to index

--- src/uikit/index.js
+++ src/uikit/index.js
@@ -1,8 +1,12 @@
 import { createCn } from 'bem-react-classname';
-import Text from './text/text';
+import Text from './text';
+import TextInput from './text-input';
+import Icon from './icon';
 
 export const uikitCn = createCn('control');
 
 export {
+    Icon,
     Text,
+    TextInput,
 }
