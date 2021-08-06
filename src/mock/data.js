0b315f4dbe6effec668c56a3cc005f4f4193ff68
Sergei Mikhailovskikh <for.mikhe@gmail.com>
2021-08-06 13:08:00 +0300

    : Add mock data

---
+++ src/mock/data.js
@@ -0,0 +1,115 @@
+import pic1 from './1.png';
+import pic2 from './2.png';
+import pic3 from './3.png';
+import pic4 from './4.png';
+import pic5 from './5.png';
+import pic6 from './6.png';
+import pic7 from './7.png';
+import pic8 from './8.png';
+import pic9 from './9.png';
+import pic10 from './10.png';
+
+
+const mockData = [
+    {
+        title: 'Pic 1',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: true,
+        src: pic1,
+    },
+    {
+        title: 'Pic 3',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: false,
+        src: pic3,
+    },
+    {
+        title: 'Pic 2 Long pic name should be there to test rendering',
+        author: 'Long author name should be there to test rendering',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: false,
+        src: pic2,
+    },
+    {
+        title: 'Pic 4',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: true,
+        src: pic4,
+    },
+    {
+        title: 'Pic 5',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: true,
+        src: pic5,
+    },
+    {
+        title: 'Pic 6',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: true,
+        src: pic6,
+    },
+    {
+        title: 'Pic 2 Long pic name should be there to test rendering',
+        author: 'Long author name should be there to test rendering',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: false,
+        src: pic2,
+    },
+    {
+        title: 'Pic 7',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: true,
+        src: pic7,
+    },
+    {
+        title: 'Pic 8',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: true,
+        src: pic8,
+    },
+    {
+        title: 'Pic 9',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: true,
+        src: pic9,
+    },
+    {
+        title: 'Pic 10',
+        author: 'Сандро Боттичелли',
+        weight: '1501 г',
+        price: '2 000 000 $',
+        sale: '1 000 000 $',
+        sold: true,
+        src: pic10,
+    },
+];
+
+export default mockData;
